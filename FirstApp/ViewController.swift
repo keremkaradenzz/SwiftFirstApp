2fb46b7b610da7ec076f53a11a68055aa8fd0022
Kerem Karadeniz <keremkaradenzz@gmail.com>
2022-05-19 15:10:52 +0300

    first commit

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onChange(_ sender: Any) {
+        imageView.image = UIImage(named: "cankan")
+        
+    }
+    
 }
 
